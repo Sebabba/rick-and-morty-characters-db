62bdb4dbc463609685c7a0f00d30d27967b1bafe
Sebastiano Silipo <sebastiano.silipo98@gmail.com>
2026-01-13 17:54:12 +0100

    [#ADD] Infinite scrolling

--- src/components/characters.tsx
+++ src/components/characters.tsx
@@ -1,5 +1,5 @@
 import type {JSX} from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef, useCallback } from 'react'
 import { Character } from '~/utils/types'
 import Card from './card'
 
@@ -7,17 +7,21 @@ export default function Characters():JSX.Element {
     const [items, setItems] = useState<Character[]>([])
     const [loading, setLoading] = useState<boolean>(false)
     const [error, setError] = useState<string | null>(null)
+    const [page, setPage] = useState<number>(1)
+    const [hasMore, setHasMore] = useState<boolean>(true)
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 setLoading(true)
-                const response = await fetch(`https://rickandmortyapi.com/api/character`);
+                const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
 
-                if(!response.ok) throw new Error("Fetch error");
+                if(!response.ok) throw new Error("Fetch error")
 
-                const data = await response.json();
-                setItems(data.results)
+                const data = await response.json()
+
+                setItems(prev => [...prev, ... data.results])
+                setHasMore(data.info.next !== null)
             } catch (err) {
                 setError(err instanceof Error ? err.message : "Unknown error")
             } finally {
@@ -25,20 +29,49 @@ export default function Characters():JSX.Element {
             }
         };
 
-        fetchData();
-    },[])
+        if (hasMore)
+            fetchData();
+    }, [page])
 
-    if (loading) return <div className="status-msg">Caricamento in corso...</div>;
-    if (error) return <div className="status-msg error">{error}</div>;
+    const observer = useRef<IntersectionObserver | null>(null)
+
+    const lastElementRef = useCallback((node: HTMLDivElement | null) => {
+        if (loading)
+            return
+
+        if (observer.current)
+            observer.current.disconnect()
+
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting && hasMore) {
+                setPage(prev => prev + 1)
+            }
+        })
+
+        if (node)
+            observer.current.observe(node)
+    }, [loading, hasMore])
 
     return(
         <>
             <h2>Characters</h2>
+
             <div className='grid-container'>
-                {items.map((item) => (
-                    <Card item={item} />
-                ))}
+                {items.map((item, index) => {
+                    if (index === items.length - 1) {
+                        return (
+                            <div ref={lastElementRef} key={item.id}>
+                                <Card item={item} />
+                            </div>
+                        )
+                    }
+
+                    return <Card key={item.id} item={item} />
+                })}
             </div>
+
+            {loading && <div className="status-msg">Caricamento...</div>}
+            {error && <div className="status-msg error">{error}</div>}
         </>
     )
 }
